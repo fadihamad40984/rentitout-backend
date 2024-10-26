83e7f051b3530ae3df5f18a03d481e07daa3ecc3
fadihamad40984 <fadihamad40984@gmail.com>
2024-10-26 20:40:57 +0200

    modified:   config/db.js new file:   .env

--- config/db.js
+++ config/db.js
@@ -8,7 +8,9 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+
 });
+
 
 connection.connect((err) => {
   if (err) {
